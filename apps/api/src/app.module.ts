41e71f4c3609e9ea59501004102d2937b5e9e033
jihwooon <jihwooon@gmail.com>
2024-04-05 14:18:15 +0900

    사용하지 않는 App 컨트롤러 및 App 서비스 제거
    사용하지 않는 AppController와 AppService 제거했습니다

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { OctokitModule } from 'nestjs-octokit';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { IssueController } from './issue/issue.controller';
 
@@ -24,7 +22,6 @@ import { IssueController } from './issue/issue.controller';
       },
     }),
   ],
-  controllers: [AppController, IssueController],
-  providers: [AppService],
+  controllers: [IssueController],
 })
 export class AppModule {}
