29d17244f86881833b38e00e15d187e347033ecb
jihwooon <jihwooon@gmail.com>
2024-04-07 00:17:09 +0900

    API 서버에 Prometheus 모니터링을 추가하라
    주요 변경 사항
    - package.json
      - @willsoto/nestjs-prometheus 패키지 and prom-client 패키지 추가 (의존성 설치)
    - app.module.ts
      - PrometheusModule.register 메서드를 사용하여 Prometheus 모니터링 통합
        - 경로: /metrics
        - 기본 메트릭 활성화

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { OctokitModule } from 'nestjs-octokit';
 import { HealthModule } from './health/health.module';
 import { IssueController } from './issue/issue.controller';
+import { MonitoringModule } from './monitoring/monitoring.module';
 
 @Module({
   imports: [
     HealthModule,
+    MonitoringModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
