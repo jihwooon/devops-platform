4b64bde7f21700f0f1608fccd370152c84f99ae6
jihwooon <jihwooon@gmail.com>
2024-04-12 12:19:35 +0900

    Prometheus 모니터링 통합 및 인터셉터를 추가하라
    - Prometheus 모니터링을 API 애플리케이션에 통합했습니다.
    - PrometheusInterceptor 를 추가하여 API 요청 처리 시간 및 오류를 모니터링합니다.
    - 이 변화는 실제 서비스 성능을 측정하는 데 도움이 됩니다.
    
    - 새로운 파일 prometheus.interceptor.ts 를 추가했습니다.
    - 이 파일은 PrometheusInterceptor 클래스를 포함하며 API 요청 처리 시간 및 오류를 모니터링합니다.
    - 또한 서비스 정보를 등록하는 registerServiceInfo 메서드도 포함되어 있습니다.

--- apps/api/src/github/issues/issue.controller.ts
+++ apps/api/src/github/issues/issue.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { OctokitService } from 'nestjs-octokit';
 import { IssueResponseDto } from './dto/issue.dto';
@@ -6,37 +6,6 @@ import { IssueResponseDto } from './dto/issue.dto';
 @Controller('git')
 export class IssueController {
   constructor(private octokitService: OctokitService) {}
-
-  /**
-   * 인증된 사용자의 로그인 이름을 가져옵니다.
-   * @returns 로그인 이름입니다.
-   */
-  @Get('/login')
-  async getLogin() {
-    const {
-      data: { login },
-    } = await this.octokitService.rest.users.getAuthenticated();
-
-    return login;
-  }
-
-  /**
-   * 제공된 저장소 이름을 기반으로 저장소의 이름을 가져옵니다.
-   * @param repoName - 저장소의 이름
-   * @returns 저장소의 이름입니다.
-   */
-  @Get('/repo')
-  async getRepo(@Query('repo_name') repoName: string) {
-    const owner = await this.getLogin();
-    const {
-      data: { name },
-    } = await this.octokitService.rest.repos.get({
-      owner,
-      repo: repoName,
-    });
-
-    return name;
-  }
 
   /**
    * 제공된 저장소 이름과 이슈 번호를 기반으로 이슈의 제목과 내용을 가져옵니다.
@@ -66,5 +35,34 @@ export class IssueController {
     });
 
     return IssueResponseDto.of(response, owner);
+  }
+
+  /**
+   * 인증된 사용자의 로그인 이름을 가져옵니다.
+   * @returns 로그인 이름입니다.
+   */
+  private async getLogin() {
+    const {
+      data: { login },
+    } = await this.octokitService.rest.users.getAuthenticated();
+
+    return login;
+  }
+
+  /**
+   * 제공된 저장소 이름을 기반으로 저장소의 이름을 가져옵니다.
+   * @param repoName - 저장소의 이름
+   * @returns 저장소의 이름입니다.
+   */
+  private async getRepo(repoName: string) {
+    const owner = await this.getLogin();
+    const {
+      data: { name },
+    } = await this.octokitService.rest.repos.get({
+      owner,
+      repo: repoName,
+    });
+
+    return name;
   }
 }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CORS_ALLOW_ORIGIN, PORT } from './config/settings';
 import { setupSwagger } from './config/swagger';
+import { PrometheusInterceptor } from './monitoring/interceptor/prometheus.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new PrometheusInterceptor());
 
   app.enableCors({
     origin: CORS_ALLOW_ORIGIN,
