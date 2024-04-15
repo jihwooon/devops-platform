2a9bb34280dbf5be1978b1121d2ced6c810732ef
jihwooon <jihwooon@gmail.com>
2024-04-15 17:00:38 +0900

    스타일 테마 적용 및 글로벌 스타일 최적화
    * theme.ts 생성하여 light & dark 테마 정의
    * ThemeProvider 사용하여 App 컴포넌트에 dark 테마 적용
    * Header 컴포넌트 스타일을 테마 색상에 따라 변경
    * GlobalStyle 개선: 테마 인터페이스 도입 및 글로벌 스타일 최적화

--- apps/vite-projects/src/App.tsx
+++ apps/vite-projects/src/App.tsx
@@ -1,12 +1,18 @@
 import Home from "./pages/Home.tsx";
 import Layout from "./components/layout/Layout.tsx";
 import "sanitize.css";
+import { GlobalStyle } from "./style/global.ts";
+import { ThemeProvider } from "styled-components";
+import { dark } from "./style/theme.ts";
 
 function App() {
   return (
-    <>
-      <Layout children={<Home />} />
-    </>
+    <ThemeProvider theme={dark}>
+      <GlobalStyle themeName={"dark"} />
+      <Layout>
+        <Home />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
